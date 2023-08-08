882e5855e879f7d06c9b89a230c3a32e8f3aacd9
ishanjit <ishanjit2003@gmail.com>
2023-08-09 01:59:05 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Center(child: Text("First Flutter")),
+          foregroundColor: Colors.black,
+        ),
+        body: const Center(
+            child: Image(
+          image: NetworkImage(
+              "https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg"),
+        ))),
+  ));
+}
